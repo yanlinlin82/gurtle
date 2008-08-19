2a1058915607f40e6a95366769b152a9053ab6df
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2008-08-19 09:14:27 +0000

    Initial import.

---
+++ trunk/src/Gurtle/Plugin.cs
@@ -0,0 +1,147 @@
+﻿#region License, Terms and Author(s)
+//
+// Gurtle - IBugTraqProvider for Google Code
+// Copyright (c) 2008 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the New BSD License, a copy of which should have 
+// been delivered along with this distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#endregion
+
+namespace Gurtle
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.Runtime.InteropServices;
+    using Interop.BugTraqProvider;
+
+    #endregion
+
+    [ ComVisible(true) ]
+    [ Guid("91974081-2DC7-4FB1-B3BE-0DE1C8D6CE4E") ]
+    [ ClassInterface(ClassInterfaceType.None) ]
+    public sealed class Plugin : IBugTraqProvider
+    {
+        public string GetCommitMessage(
+            IntPtr hParentWnd, 
+            string parameters, string commonRoot, string[] pathList,
+            string originalMessage)
+        {
+            return GetCommitMessage(WindowHandleWrapper.TryCreate(hParentWnd), parameters, originalMessage);
+        }
+
+        [ ComVisible(false) ]
+        public static string GetCommitMessage(
+            IWin32Window parentWindow, 
+            string parameters, string originalMessage)
+        {
+            try
+            {
+                var settings = ParseParameters(parameters)
+                    .ToDictionary(p => p.Key, p => p.Value, StringComparer.OrdinalIgnoreCase);
+
+                string project;
+                if (!settings.TryGetValue("project", out project) || project.Length == 0)
+                    throw new ApplicationException("Missing Google Code project specification.");
+
+                string userName, status;
+                settings.TryGetValue("user", out userName);
+                settings.TryGetValue("status", out status);
+
+                var dialog = new IssueBrowserDialog
+                {
+                    Project = project, 
+                    UserNamePattern = userName, 
+                    StatusPattern = status,
+                    UpdateCheckEnabled = true,
+                };
+
+                if (dialog.ShowDialog(parentWindow) != DialogResult.OK
+                    || dialog.SelectedIssueObjects.Count == 0)
+                {
+                    return originalMessage;
+                }
+
+                var message = new StringBuilder(originalMessage);
+
+                if (originalMessage.Length > 0 && !originalMessage.EndsWith("\n"))
+                    message.AppendLine();
+
+                foreach (var issue in dialog.SelectedIssueObjects)
+                {
+                    message.Append("Fixed #")
+                           .Append(issue.Id).Append(": ")
+                           .AppendLine(issue.Summary);
+                }
+                
+                return message.ToString();
+            }
+            catch (Exception e)
+            {
+                ShowErrorBox(parentWindow, e);
+                throw;
+            }
+        }
+        
+        public bool ValidateParameters(IntPtr hParentWnd, string parameters)
+        {
+            return ValidateParameters(WindowHandleWrapper.TryCreate(hParentWnd), parameters);
+        }
+
+        [ ComVisible(false) ]
+        public bool ValidateParameters(IWin32Window parentWindow, string parameters)
+        {
+            return true; // TODO validation
+        }
+
+        public string GetLinkText(IntPtr hParentWnd, string parameters)
+        {
+            return "Select Issue";
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> ParseParameters(string parameters)
+        {
+            return ParseParameters(parameters.Split(';'));
+        }
+        
+        private static IEnumerable<KeyValuePair<string, string>> ParseParameters(IEnumerable<string> parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                var pair = parameter.Split(new[] {'='}, 2);
+                var key = pair[0].Trim();
+                var value = pair.Length > 1 ? pair[1].Trim() : string.Empty;
+                yield return new KeyValuePair<string, string>(key, value);
+            }
+        }
+
+        private static void ShowErrorBox(IWin32Window parent, Exception e)
+        {
+            MessageBox.Show(parent, e.Message, 
+                e.Source + ": " + e.GetType().Name, 
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ trunk/uninstall.cmd
@@ -0,0 +1,44 @@
+@echo off
+
+REM Gurtle
+REM Copyright (c) 2008 Atif Aziz. All rights reserved.
+REM
+REM  Author(s):
+REM
+REM      Atif Aziz, http://www.raboof.com
+REM
+REM This library is free software; you can redistribute it and/or modify it 
+REM under the terms of the New BSD License, a copy of which should have 
+REM been delivered along with this distribution.
+REM
+REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+REM "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+REM LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+REM PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+REM OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+REM SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+REM LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+REM DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+REM THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+REM (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+REM OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+setlocal
+
+echo This command script will remove the registry keys that were used
+echo to install and publish Gurtle in the system and as an issue tracker 
+echo plug-in for TortoiseSNV. You must have administrative access to
+echo the system registry in order for this script to succeed.
+echo.
+
+set /p reply=Continue (Y/N)?
+if "%reply%"=="Y" goto uninstall
+if "%reply%"=="y" goto uninstall
+if "%reply%"=="yes" goto uninstall
+goto :eof
+
+:uninstall
+reg delete HKCR\Gurtle.Plugin
+reg delete HKCR\CLSID\{91974081-2DC7-4FB1-B3BE-0DE1C8D6CE4E}
+
+
