4db5c6a29f7aa2cf7f349813d5ba6bde58bff281
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2008-08-31 15:04:33 +0000

    Tagged release 0.2.

---
+++ tags/REL-0.2/src/Gurtle/Properties/AssemblyInfo.cs
@@ -0,0 +1,53 @@
+﻿#region License, Terms and Author(s)
+//
+// Gurtle - IBugTraqProvider for Google Code
+// Copyright (c) 2008 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the New BSD License, a copy of which should have 
+// been delivered along with this distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#endregion
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gurtle")]
+[assembly: AssemblyDescription("Issue tracker plug-in for projects hosted by Google Code")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gurtle")]
+[assembly: AssemblyCopyright("Copyright (c) Atif Aziz. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("944193f5-15b9-4468-9a54-572f68b9ce5a")]
+
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.10331.1421")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("RELEASE")]
+#endif
---
+++ tags/REL-0.2/src/Gurtle/TextBoxExtensions.cs
@@ -0,0 +1,56 @@
+﻿#region License, Terms and Author(s)
+//
+// Gurtle - IBugTraqProvider for Google Code
+// Copyright (c) 2008 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the New BSD License, a copy of which should have 
+// been delivered along with this distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#endregion
+
+namespace Gurtle
+{
+    using System.Windows.Forms;
+
+    internal static class TextBoxExtensions
+    {
+        /// <remarks>
+        /// According to <see cref="TextBoxBase.ShortcutsEnabled"/> docs, 
+        /// <see cref="TextBox"/> does not support shortcuts and therefore 
+        /// the useful <kbd>CTRL+A</kbd> shortcut to select all text does 
+        /// not work. This method enables that shortcut. Do not call
+        /// this method more than once for the same <see cref="TextBox"/> 
+        /// instance.</remarks>
+
+        internal static void EnableShortcutToSelectAllText(this TextBox textBox)
+        {
+            textBox.KeyPress += (sender, e) =>
+            {
+                //
+                // Credit: http://www.redmountainsw.com/wordpress/archives/shortcutsenabled-on-textbox-winforms-dotnet-does-not-perform-selectall-with-controla
+                //
+
+                if (e.KeyChar != 1) return;
+                textBox.SelectAll();
+                e.Handled = true;
+            };
+        }
+    }
+}
