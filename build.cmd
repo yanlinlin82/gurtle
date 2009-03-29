f90452a3dacba1d104343e8c8e816071662fe38c
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-03-29 18:08:40 +0000

    Added MS Build path to build batch script so it can be used without setting up the path environment.

--- build.cmd
+++ build.cmd
@@ -24,5 +24,5 @@ REM (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 REM OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 for %%i in (Debug Release) do (
-    msbuild /p:Configuration=%%i "%~dp0src\Gurtle.sln"
+    "%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild" /p:Configuration=%%i "%~dp0src\Gurtle.sln"
 )
