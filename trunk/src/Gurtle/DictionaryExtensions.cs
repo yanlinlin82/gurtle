dc77022e225d41de3522ff42ddabf3fd240ac8d3
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-04-05 19:18:50 +0000

    Added extension methods for popping a value off a dictionary.

--- trunk/src/Gurtle/DictionaryExtensions.cs
+++ trunk/src/Gurtle/DictionaryExtensions.cs
@@ -41,5 +41,24 @@ namespace Gurtle
             V value;
             return dict.TryGetValue(key, out value) ? value : @default;
         }
+
+        public static V Pop<K, V>(this IDictionary<K, V> dict, K key)
+        {
+            var value = dict[key];
+            dict.Remove(key);
+            return value;
+        }
+
+        public static V TryPop<K, V>(this IDictionary<K, V> dict, K key)
+        {
+            return TryPop(dict, key, default(V));
+        }
+
+        public static V TryPop<K, V>(this IDictionary<K, V> dict, K key, V @default)
+        {
+            var value = dict.Find(key, @default);
+            dict.Remove(key);
+            return value;
+        }
     }
 }
