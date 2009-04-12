852179bf096145c27f92dce168178102eb2be1e2
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-04-12 13:25:47 +0000

    Added initial dialog box in preparation of issue #16.

---
+++ src/Gurtle/IssueUpdateDialog.cs
@@ -0,0 +1,20 @@
+﻿namespace Gurtle
+{
+    using System.Windows.Forms;
+
+    public partial class IssueUpdateDialog : Form
+    {
+        public IssueUpdateDialog()
+        {
+            InitializeComponent();
+
+            var page = new TabPage("Issue #1");
+            page.Controls.Add(new IssueUpdatePage() { Dock = DockStyle.Fill });
+            tabs.TabPages.Add(page);
+
+            page = new TabPage("Issue #1");
+            page.Controls.Add(new IssueUpdatePage() { Dock = DockStyle.Fill });
+            tabs.TabPages.Add(page);
+        }
+    }
+}
