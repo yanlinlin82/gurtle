ed8befa1efa9511dc6692ea9cea8ddfb4d538637
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-04-28 16:44:58 +0000

    Added icon to issue browser dialog.

--- trunk/src/Gurtle/IssueBrowserDialog.Designer.cs
+++ trunk/src/Gurtle/IssueBrowserDialog.Designer.cs
@@ -287,6 +287,7 @@
             this.Controls.Add(this.detailButton);
             this.Controls.Add(this.issueListView);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MinimizeBox = false;
             this.Name = "IssueBrowserDialog";
