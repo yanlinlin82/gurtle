17c0a88f4ed5ae8a5f73f3dc78b8004f70ca04b9
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-04-28 16:12:43 +0000

    Made the Python script self-executable.

--- tools/amadmin.cmd
+++ tools/amadmin.cmd
@@ -1,1 +1,16 @@
-@"%~dp0IronPython\ipy" "%~dpn0.py" %*
+@"%~dp0IronPython\ipy" -x "%~f0" %* & goto :EOF
+import sys
+from System import Environment
+from System.Security.Principal import \
+    WindowsIdentity, WindowsPrincipal, WindowsBuiltInRole
+
+def main():
+    user = WindowsPrincipal(WindowsIdentity.GetCurrent())
+    if user.IsInRole(WindowsBuiltInRole.Administrator):
+        print 'You are a system administrator.'
+    else:
+        print 'You are NOT a system administrator.'
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
