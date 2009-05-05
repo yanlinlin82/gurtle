dcdf989783fc7b47514f6ef7bf5a43ef0a30cc50
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-05-05 21:44:00 +0000

    Added string formatting and pair-splitting extension methods.

--- trunk/src/Gurtle/StringExtensions.cs
+++ trunk/src/Gurtle/StringExtensions.cs
@@ -29,7 +29,12 @@ namespace Gurtle
 {
     #region Imports
 
+    using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Globalization;
+    using System.Text;
+    using System.Web;
 
     #endregion
 
@@ -134,6 +139,178 @@ namespace Gurtle
 
             return sliceLength > 0 ?
                 str.Substring(start, sliceLength) : string.Empty;
+        }
+
+        public static string FormatWith(this string format, params object[] args)
+        {
+            return format.FormatWith(null, null, args);
+        }
+
+        public static string FormatWith(this string format, IFormatProvider provider, params object[] args)
+        {
+            return format.FormatWith(provider, null, args);
+        }
+
+        public static string FormatWith(this string format, Func<string, object[], IFormatProvider, string> binder, params object[] args)
+        {
+            return format.FormatWith(null, binder, args);
+        }
+
+        public static string FormatWith(this string format,
+            IFormatProvider provider, Func<string, object[], IFormatProvider, string> binder, params object[] args)
+        {
+            return format.FormatWithImpl(provider, binder ?? FormatTokenBinder, args);
+        }
+
+        private static string FormatWithImpl(this string format,
+            IFormatProvider provider, Func<string, object[], IFormatProvider, string> binder, params object[] args)
+        {
+            if (format == null)
+                throw new ArgumentNullException("format");
+
+            Debug.Assert(binder != null);
+
+            var result = new StringBuilder(format.Length * 2);
+            var token = new StringBuilder();
+
+            var e = format.GetEnumerator();
+            while (e.MoveNext())
+            {
+                var ch = e.Current;
+                if (ch == '{')
+                {
+                    while (true)
+                    {
+                        if (!e.MoveNext())
+                            throw new FormatException();
+
+                        ch = e.Current;
+                        if (ch == '}')
+                        {
+                            if (token.Length == 0)
+                                throw new FormatException();
+
+                            result.Append(binder(token.ToString(), args, provider));
+                            token.Length = 0;
+                            break;
+                        }
+                        if (ch == '{')
+                        {
+                            result.Append(ch);
+                            break;
+                        }
+                        token.Append(ch);
+                    }
+                }
+                else if (ch == '}')
+                {
+                    if (!e.MoveNext() || e.Current != '}')
+                        throw new FormatException();
+                    result.Append('}');
+                }
+                else
+                {
+                    result.Append(ch);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static string FormatTokenBinder(string token, object[] args, IFormatProvider provider)
+        {
+            Debug.Assert(token != null);
+
+            var source = args[0];
+            var dotIndex = token.IndexOf('.');
+            int sourceIndex;
+            if (dotIndex > 0 && int.TryParse(token.Substring(0, dotIndex), NumberStyles.None, CultureInfo.InvariantCulture, out sourceIndex))
+            {
+                source = args[sourceIndex];
+                token = token.Substring(dotIndex + 1);
+            }
+
+            var format = string.Empty;
+
+            var colonIndex = token.IndexOf(':');
+            if (colonIndex > 0)
+            {
+                format = "{0:" + token.Substring(colonIndex + 1) + "}";
+                token = token.Substring(0, colonIndex);
+            }
+
+            if (int.TryParse(token, NumberStyles.None, CultureInfo.InvariantCulture, out sourceIndex))
+            {
+                source = args[sourceIndex];
+                token = null;
+            }
+
+            object result;
+
+            try
+            {
+                result = source.DataBind(token) ?? string.Empty;
+            }
+            catch (HttpException e)
+            {
+                throw new FormatException(e.Message, e);
+            }
+
+            return !string.IsNullOrEmpty(format)
+                 ? string.Format(provider, format, result)
+                 : result.ToString();
+        }
+
+        /// <summary>
+        /// Splits a string into a key and a value part using a specified 
+        /// character to separate the two.
+        /// </summary>
+        /// <remarks>
+        /// The key or value of the resulting pair is never <c>null</c>.
+        /// </remarks>
+
+        public static KeyValuePair<string, string> SplitPair(this string str, char separator)
+        {
+            if (str == null) throw new ArgumentNullException("str");
+
+            return SplitPair(str, str.IndexOf(separator), 1);
+        }
+
+        /// <summary>
+        /// Splits a string into a key and a value part using any of a 
+        /// specified set of characters to separate the two.
+        /// </summary>
+        /// <remarks>
+        /// The key or value of the resulting pair is never <c>null</c>.
+        /// </remarks>
+
+        public static KeyValuePair<string, string> SplitPair(this string str, params char[] separators)
+        {
+            if (str == null) throw new ArgumentNullException("str");
+
+            return separators == null || separators.Length == 0
+                 ? new KeyValuePair<string, string>(str, string.Empty)
+                 : SplitPair(str, str.IndexOfAny(separators), 1);
+        }
+
+        /// <summary>
+        /// Splits a string into a key and a value part by removing a
+        /// portion of the string.
+        /// </summary>
+        /// <remarks>
+        /// The key or value of the resulting pair is never <c>null</c>.
+        /// </remarks>
+
+        public static KeyValuePair<string, string> SplitPair(this string str, int index, int count)
+        {
+            if (str == null) throw new ArgumentNullException("str");
+            if (count <= 0) throw new ArgumentOutOfRangeException("count", count, null);
+
+            return new KeyValuePair<string, string>(
+                /* key   */ index < 0 ? str : str.Substring(0, index),
+                /* value */ index < 0 || index + 1 >= str.Length
+                            ? string.Empty
+                            : str.Substring(index + count));
         }
     }
 }
