78fba0539a7e445e1486c7123fa07761f68d2b40
azizatif <azizatif@a4816920-c454-0410-890f-b1def2fa94d2>
2009-05-09 15:39:49 +0000

    Fixed issue #34, "Issue update fails under TSVN."

---
+++ src/Gurtle/AppPaths.cs
@@ -0,0 +1,62 @@
+﻿#region License, Terms and Author(s)
+//
+// Gurtle - IBugTraqProvider for Google Code
+// Copyright (c) 2008 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the New BSD License, a copy of which should have 
+// been delivered along with this distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+#endregion
+
+namespace Gurtle
+{
+    #region Imports
+
+    using System;
+    using System.IO;
+    using System.Reflection;
+
+    #endregion
+
+    internal static class AppPaths
+    {
+        public static string GetLocal()
+        {
+            return GetLocal(null);
+        }
+
+        public static string GetLocal(string path)
+        {
+            return !string.IsNullOrEmpty(path) && Path.IsPathRooted(path) 
+                 ? path : GetLocalImpl(path);
+        }
+
+        private static string GetLocalImpl(string tailPath)
+        {
+            var localAppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            var assemblyName = Assembly.GetExecutingAssembly().GetName();
+            var appVersionedPath = string.Format(@"{0}\{1}", assemblyName.Name, assemblyName.Version.ToString(2));
+            var path = Path.Combine(localAppDataPath, appVersionedPath);
+            if (!string.IsNullOrEmpty(tailPath))
+                Path.Combine(path, tailPath);
+            return path;
+        }
+    }
+}
